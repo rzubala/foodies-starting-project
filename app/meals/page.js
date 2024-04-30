abb167d9327f930ca50a83f1842fc7d51f10b194
Rafał Zubala <rzubala@gmail.com>
2024-04-30 13:55:28 +0200

    meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,26 @@
-export default function MealsPage() {
-    return (<h1>Meals</h1>)
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import Link from 'next/link';
+import classes from './page.module.css';
+
+export default async function MealsPage() {
+
+    const meals = await getMeals();
+
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favourite recipe, and cook by yourself</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share your favourite meal</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
+    );
 }
