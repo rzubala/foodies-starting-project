85ef21950d848517517521309d360be20c503f04
Rafał Zubala <rzubala@gmail.com>
2024-04-30 12:46:08 +0200

    css tuning

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -8,6 +8,6 @@ export default function NavLink({href, children}) {
     const path = usePathname();
 
     return (
-        <Link href={href} className={path.startsWith(href) ? classes.active : undefined} >{children}</Link>
+        <Link href={href} className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link} >{children}</Link>
     );
 }
